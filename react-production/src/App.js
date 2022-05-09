6085e013e83239b2864eb51ae576a7c00d07e50b
vu van dao <vvdao096@gmail.com>
2022-05-09 13:56:23 +0700

    test build action

--- react-production/src/App.js
+++ react-production/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test auto build with github actions
+          Hello A Nam 
         </a>
       </header>
     </div>
